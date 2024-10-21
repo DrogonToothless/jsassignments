4d20b40568a20ee259faf36723f4dc8bc8e26075
James Lawrence <jamesblawrence2007@gmail.com>
2024-10-21 10:06:43 -0600

    completed queue implementation

--- data-structures/src/queue.js
+++ data-structures/src/queue.js
@@ -1,17 +1,18 @@
 class Queue {
   constructor() {
     this.data = [];
+    this.otherData = [];
   }
-
   enqueue(element) {
+    this.data.push(element);
   }
-
   dequeue() {
+    this.data.pop();
   }
-
   peek() {
+    return this.data[this.data.length - 1];
   }
-
   size() {
+    return this.data.length;
   }
 }
--- data-structures/src/rpn.js
+++ data-structures/src/rpn.js
@@ -13,6 +13,20 @@ Examples:
 Note: the expression will always have space separated 
       inputs like "4 5 *" rather than "45*" or "4 4*"
  */
-
 function rpn(expression) {
+    const operators = new Set(['+', '-', '*', '/']);
+    const stack = [];
+    const tokens = expression.split(' ');
+    for (const token of tokens) {
+        if (operators.has(token)) {
+            const a = stack.pop();
+            const b = stack.pop();
+            const expr = `${a} ${token} ${b}`;
+            const result = eval(expr);
+            stack.push(result);
+        } else {
+            stack.push(token);
+        }
+    }
+    return stack.length === 1 ? stack[0] : 'Invalid RPN';
 }
